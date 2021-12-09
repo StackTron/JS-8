0b817119afd5e34bd3906345efe7753f13fbad7b
Roman <88782752+StackTron@users.noreply.github.com>
2021-12-09 17:57:14 +0300

    JS-8

---
+++ basket.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const basketCountEl = document.querySelector('.cartIconWrap span');
+const basketValueEl = document.querySelector('.basketTotalValue');
+const totalBask = document.querySelector('.basketTotal');
+const basketEl = document.querySelector('.basket');
+
+document.querySelector('.cartIconWrap').addEventListener('click', () => {
+    basketEl.classList.toggle('hidden');
+});
+
+const basket = {};
+
+
+document.querySelector('.featuredItems').addEventListener('click', event => {
+    if (!event.target.closest('.addToCrd')) {
+        return;
+    }
+    const featuredItem = event.target.closest('.featuredItem');
+    const id = +featuredItem.dataset.id;
+    const name = featuredItem.dataset.name;
+    const price = +featuredItem.dataset.price;
+    addToCrd(id, name, price);
+});
+
+function addToCrd(id, name, price) {
+    if (!(id in basket)) {
+        basket[id] = {id: id, name: name, price: price, count: 0};
+    }
+    basket[id].count++;
+    basketCountEl.textContent = getTotalBasketCount().toString();
+    basketValueEl.textContent = getTotalBasketPrice().toFixed(2);
+    renderProductInBasket(id);
+}
+
+function getTotalBasketCount() {
+    return Object.values(basket).reduce((acc, product) => acc + product.count, 0);
+}
+
+function getTotalBasketPrice() {
+    return Object.values(basket)
+    .reduce((acc, product) => acc + product.count * product.price, 0)
+}
+
+function renderProductInBasket(id) {
+    const basketRowEl = basketEl
+    .querySelector(`.basketRow[data-productId]="${id}"`);
+    if (!basketRowEl) {
+        renderNewProductInBasket(id);
+        return;
+    }
+
+    basketRowEl.querySelector('.productCount').textContent = basket[id].count;
+    basketRowEl.querySelector('.productTotalRow').textContent = basket[id].count * basket[id].price;
+}
+
+function renderNewProductInBasket(productId) {
+    const productRow = `
+    <div class="basketRow" data-productId="${productId}">
+    <div>${basket[productId].name}</div>
+    <div>
+        <span class="productCount">${basket[productId].count}</span> шт.
+    </div>
+    <div>$${basket[productId].price}</div>
+    <div>
+        $<span class="productTotalRow">${(basket[productId].price * basket[productId].count).toFixed(2)}</span>
+    </div>
+    </div>
+    `;
+    totalBask.insertAdjacentHTML("beforebegin", productRow);
+}
